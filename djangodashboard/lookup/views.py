cf9b5a2a6d62318a7cf5a17927a67b79569f6473
Erik Pohl <epohl@dmigs.com>
2024-12-25 16:25:18 -0500

    added a jumbotron and api call

--- djangodashboard/lookup/views.py
+++ djangodashboard/lookup/views.py
@@ -3,7 +3,16 @@ from . import views
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&api_key=02A407DD-7F9F-4D01-ADAD-A582AD35789F&zipCode=02474")
+    try:
+        api = json.loads(api_request.content)
+    except:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
