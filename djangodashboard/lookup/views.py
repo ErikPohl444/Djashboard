2863af4685ce77c3e09b7795e364a896bdc1fe34
Erik Pohl <epohl@dmigs.com>
2025-01-01 09:44:16 -0500

    made every display item controlled by env json so i can have multople dashboard types with same engine

--- djangodashboard/lookup/views.py
+++ djangodashboard/lookup/views.py
@@ -13,9 +13,6 @@ def home(request):
 
     api_calls = []
     api_transformations = []
-    with open("env.json") as json_handle:
-        cfg = json.load(json_handle)
-    api_key = cfg["api_key"]
     with open("env2.json") as json_handle:
         cfg2 = json.load(json_handle)
         cfg2 = cfg2["api_data"]
@@ -33,9 +30,9 @@ def home(request):
     if request.method == "POST":
         zipcode = request.POST['zipcode']
     else:
-        zipcode = cfg["initial_zip"]
-        # right now do nothing with zipcode
-
+        zipcode = None
+    # make a filter here
+    
     # extract and transform
     dashboard_vals = []
     for api_no, api_call in enumerate(api_calls):
@@ -51,19 +48,15 @@ def home(request):
             cat_rec = fun(api)
         except KeyError:
             api = "Error..."
-
-        translate_cat_to_descr = {
-            "Good": "air quality is good",
-            "Moderate": "getting moderate",
-            "Unhealthy for Sensitive Groups": "be careful out there if you are in a sensitive group",
-            "Very Unhealthy": "everyone stay inside for now",
-            "Hazardous": "Air quality is hazardous."
-        }
-        category_color = cat_rec.lower().replace(" ", "")
-        category_description = translate_cat_to_descr[cat_rec]
-        category_subtext = f"Current {api[0]['ReportingArea']} Air quality: {api[0]['AQI']}"
         category_name = cat_rec
+        fun = lambda jresult: eval(api_transformations[api_no]["cat_subtext"])
+        category_subtext = fun(api)
+        cat_color_matrix = api_transformations[api_no]["color_translate"]
+        category_color = cat_color_matrix[cat_rec]
+        cat_descr_matrix = api_transformations[api_no]["descr_translate"]
+        category_description = cat_descr_matrix[cat_rec]
         api_status = api
+        # we have our 4 values for each widget
         dashboard_val = {
             'category_color': category_color,
             'category_description': category_description,
